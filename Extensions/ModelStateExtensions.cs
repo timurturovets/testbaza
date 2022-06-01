dd2ea424f1b487210fbb42ee774303b571c4218d
Timur <timurturovets@mail.ru>
2022-06-01 10:34:24 +0300

    Мелкие фиксы

--- Extensions/ModelStateExtensions.cs
+++ Extensions/ModelStateExtensions.cs
@@ -1,16 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
 
-namespace TestBaza.Extensions
+namespace TestBaza.Extensions;
+
+public static class ModelStateExtensions
 {
-    public static class ModelStateExtensions
+    public static IEnumerable<string> ToStringEnumerable(this ModelStateDictionary modelState)
     {
-        public static IEnumerable<string> ToStringEnumerable(this ModelStateDictionary modelState)
-        {
-            return modelState.Select(entry => {
-                var query = entry.Value?.Errors.Select(e => e.ErrorMessage);
-                if (query is null || !query.Any()) return string.Empty;
-                else return query.Aggregate((x, y) => x + ", " + y.ToLower()[0] + y[1..]);
-            }).Select(value => value + ". ");
-        }
+        return modelState.Select(entry => {
+            var query = entry.Value?.Errors.Select(e => e.ErrorMessage).ToList();
+            if (query is null || !query.Any()) return string.Empty;
+            return query.Aggregate((x, y) => x + ", " + y.ToLower()[0] + y[1..]);
+        }).Select(value => value + ". ");
     }
 }
